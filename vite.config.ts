90622fb99da06fcf88e1f701746a89e2ecd85049
Alexander Lavrynenko <lavrynenko.alexander@gmail.com>
2025-02-09 12:31:08 +0100

    add Vitest for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      exclude: ["vite.config.ts", "src/main.ts", "src/vite-env.d.ts"],
+    },
+  },
+});
